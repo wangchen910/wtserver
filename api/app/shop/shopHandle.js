5ea48cb4cf1ed8a4fc91dc19d4794779d1be3561
1098620475@qq.com <1098620475@qq.com>
2018-12-31 15:47:21 +0800

    get details id

--- api/app/shop/shopHandle.js
+++ api/app/shop/shopHandle.js
@@ -15,6 +15,9 @@ exports.buy = async function(action, session, callback){
 
 exports.getCommodity = async function(action, session, callback) {
 	 var query = {}
+   if (action.id) {
+    query.id = action.id
+   }
 	 mongo.db(fields.DEFAULT_DB).collection(fields.COMMODITY).findOne(query,function(err,data){
        if (!err) {
          callback({success:true, data: data})
