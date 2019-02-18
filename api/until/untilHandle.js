82c050ec27055fca5b251c503bfb4e2fd7ba3eea
1098620475@qq.com <1098620475@qq.com>
2019-02-18 16:28:36 +0800

    小程序码page

--- api/until/untilHandle.js
+++ api/until/untilHandle.js
@@ -134,7 +134,7 @@ exports.getQrImage = async function(obj){
       body: {
         "width": 100,
         "scene": 'wangtao',
-        "page": 'miniprogram/pages/home/index'
+        "page": 'pages/activity/index'
       }
     }, function(error, response, body) {
         console.log(body)
