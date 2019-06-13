3b559ec0bdb4662bfec3e6b8499a71bb97ce67e0
wangtao <l>
2019-06-13 22:42:29 +0800

    数据库更新

--- serverConfig.js
+++ serverConfig.js
@@ -4,20 +4,20 @@ module.exports={
 	// appId: 'wx68d1741f811385c1',
   //   secret: 'cccf54eddc93ed32382ce19ccb498ede',
 
-	// mongo:{
-  //    dbIp:'117.78.45.173',
-  //    dbPort: 27017,
-  //    dbName: 'admin',
-  //    dbUserName: 'admin',
-  //    dbPassword: 'admin'
-	// },
 	mongo:{
-     dbIp:'114.115.206.125',
+     dbIp:'117.78.45.173',
      dbPort: 27017,
      dbName: 'admin',
      dbUserName: 'admin',
      dbPassword: 'admin'
 	},
+	// mongo:{
+    //  dbIp:'114.115.206.125',
+    //  dbPort: 27017,
+    //  dbName: 'admin',
+    //  dbUserName: 'admin',
+    //  dbPassword: 'admin'
+	// },
 	redis:{
     redisIp: "117.78.45.173",
 	  redisPort: 6378,
